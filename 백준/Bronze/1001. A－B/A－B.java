b91a2895bd30d7229f03fa00659edefdcea2132b
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-06-04 13:05:41 +0900

    [Bronze V] Title: A-B, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -6,10 +6,12 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+ 
+ 		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		
-		System.out.println(a-b); 
+		System.out.println(a-b);
     }
 }
