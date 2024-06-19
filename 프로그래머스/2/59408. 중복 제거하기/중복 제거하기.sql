ece5a6bf03ab6e9d83987693a5687282792ee14d
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-06-19 17:46:17 +0900

    Update 중복 제거하기.sql

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,11 +1,7 @@
 -- 코드를 입력하세요
 SELECT
-  count(NAME) as "count"
-  from 
-  (
-      select distinct name 
-        from ANIMAL_INS
-       where NAME IS NOT NULL
-  ) as cnt
- # order by name
+select count(distinct name) as "count"
+  from ANIMAL_INS
+ where NAME IS NOT NULL
+ ;
  ;
