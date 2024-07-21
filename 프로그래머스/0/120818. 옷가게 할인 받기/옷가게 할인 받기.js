71aa9ccdfad9de6ad6f9ad3cd0ae08ebfd95737d
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-07-21 22:16:44 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+function solution(price) {
+    var answer = price;
+    if(price >= 500000){
+        answer = price - (price * 0.2) ;
+    }else if(price >= 300000){
+        answer = price - (price * 0.1) ;
+    }else if(price >= 100000){
+        answer = price - (price * 0.05) ;
+    }
+    
+    return parseInt(answer);
+}
