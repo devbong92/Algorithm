247915a9bb3c65f550696112135f538ec573143b
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-08-01 10:50:35 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT
+      cp1.CART_ID
+  FROM CART_PRODUCTS as cp1
+ inner join CART_PRODUCTS as cp2
+         on cp1.cart_id = cp2.cart_id
+      WHERE cp1.NAME = 'Milk'
+        and cp2.name = 'Yogurt'
+ group by cp1.cart_id
+ ;
