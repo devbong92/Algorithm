285158167f85ae324801a922ea56219703757914
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-08-01 13:08:21 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT
+    users.USER_ID,
+    users.NICKNAME,
+    CONCAT(users.CITY,' ', users.STREET_ADDRESS1,' ', users.STREET_ADDRESS2) AS "전체주소",
+    CONCAT(substr(users.TLNO,1,3),'-',substr(users.TLNO,4,4),'-',substr(users.TLNO,8,4)) AS "전화번호"
+  FROM USED_GOODS_USER users
+ INNER JOIN ( 
+        SELECT WRITER_ID, COUNT(BOARD_ID) AS cnt 
+          FROM USED_GOODS_BOARD
+         GROUP BY WRITER_ID
+ ) boards
+    ON users.USER_ID = boards.WRITER_ID
+ WHERE boards.cnt >= 3
+ ORDER BY users.USER_ID DESC ; 
