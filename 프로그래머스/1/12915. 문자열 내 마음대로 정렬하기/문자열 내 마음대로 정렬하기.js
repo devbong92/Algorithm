d45ce4455d3fc2b823050ad89107955e8455fc00
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-08-13 11:58:59 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 470.18 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -3,3 +3,17 @@ function solution(strings, n) {
     answer = strings.sort((a,b) => a.split('')[n].localeCompare(b.split('')[n]) || a.localeCompare(b) );
     return answer;
 }
+
+// function solution(strings, n){
+//     let arr = new Array();
+
+//     for(const str of strings){
+//         const key = str.slice(n,n+1);
+//         arr.push(key+str);
+//     }
+//     arr = arr.sort() ;
+//     for(let i = 0; i < arr.length; i++ ){
+//         arr[i] = arr[i].slice(1,arr[i].length);
+//     }
+//     return arr ;
+// }
