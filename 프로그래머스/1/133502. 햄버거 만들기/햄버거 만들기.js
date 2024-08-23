d89bdb4a5509336215361632cae84e9547f1c6ce
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-08-23 09:50:38 +0900

    [level 1] Title: 햄버거 만들기, Time: 45.55 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,44 +1,18 @@
-// function solution(ingredient) {
-//     let stk = [];
-//     let count = 0;
-//     for (let i = 0; i < ingredient.length; i++) {
-//         console.log("loop : ",stk);
-//         stk.push(ingredient[i]);
-        
-//         if (
-//             stk[stk.length-1] === 1 &&
-//             stk[stk.length-2] === 3 &&
-//             stk[stk.length-3] === 2 &&
-//             stk[stk.length-4] === 1
-//         ) {
-//             count++;
-//             console.log("front : ",stk);
-//             stk.splice(-4);
-//             console.log("back : ",stk);
-//         }
-//     }
-//     return count;
-// }
-
-
 function solution(ingredient) {
-    var answer = 0;
-    const pattern = [1, 2, 3, 1];
-    
-    for(let i = 0; i <= ingredient.length - pattern.length; i++){
-        let match = true;
-        for(let j = 0; j < pattern.length; j++){
-            if(pattern[j] !== ingredient[i + j]) {
-                match = false;
-                break;
-            }
-        }
-        if(match){
-            answer += 1;
-            ingredient.splice(i, 4);
-            i -= 3;
+    let stk = [];
+    let count = 0;
+    for (let i = 0; i < ingredient.length; i++) {
+        stk.push(ingredient[i]);
+        
+        if (
+            stk[stk.length-1] === 1 &&
+            stk[stk.length-2] === 3 &&
+            stk[stk.length-3] === 2 &&
+            stk[stk.length-4] === 1
+        ) {
+            count++;
+            stk.splice(-4);
         }
     }
-    
-    return answer;
+    return count;
 }
