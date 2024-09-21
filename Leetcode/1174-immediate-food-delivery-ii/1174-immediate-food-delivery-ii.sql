1c4bd99c20da23f6871f67a368c6c09aa1e49eb9
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-09-21 12:55:54 +0900

    Time: 525 ms (90.72%), Space: 0B (100%) - LeetHub

---
+++ Leetcode/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+
+select
+    round(count(m.customer_id)/count(distinct d.customer_id) * 100,2) as immediate_percentage
+from Delivery d 
+left outer join (
+            select 
+            customer_id, 
+            min(order_date) as order_date
+            from Delivery
+            Group by customer_id
+        ) m
+on d.customer_id = m.customer_id
+and d.customer_pref_delivery_date = m.order_date
