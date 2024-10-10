1c7c02c99933b03e077860f14a7094e57b4bff3c
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-10-10 09:59:34 +0900

    Time: 369 ms (31.45%), Space: 0B (100%) - LeetHub

--- Leetcode/1321-restaurant-growth/1321-restaurant-growth.sql
+++ Leetcode/1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,19 +1,19 @@
 SELECT
-    visited_on,
+    c.visited_on,
     (
         SELECT SUM(amount)
-        FROM customer
-        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+          FROM customer
+         WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
     ) AS amount,
     ROUND(
         (
             SELECT SUM(amount) / 7
-            FROM customer
-            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+              FROM customer
+             WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
         ),2 ) AS average_amount
-FROM customer c
-WHERE visited_on >= (
+   FROM customer c
+  WHERE visited_on >= (
         SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
         FROM customer
-    )
-GROUP BY visited_on;
+        )
+  GROUP BY c.visited_on;
