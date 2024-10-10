c4e2312c91b7eaa49f9f2ca93c41598b8f64211e
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2024-10-10 09:58:25 +0900

    Time: 319 ms (74.46%), Space: 0B (100%) - LeetHub

--- Leetcode/1321-restaurant-growth/1321-restaurant-growth.sql
+++ Leetcode/1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,35 +1,19 @@
-# Write your MySQL query statement below
-
--- SELECT
---     c.visited_on,
---     (
---         SELECT SUM(amount)
---         FROM Customer
---         WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
---     ) AS amount,
---     ROUND(
---         (
---             SELECT SUM(amount) / 7
---             FROM Customer
---             WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
---         ), 2
---     ) AS average_amount
--- FROM Customer c
--- WHERE visited_on >= (
---         SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
---         FROM Customer
---     )
--- GROUP BY visited_on ;
-
-SELECT 
-    t.visited_on, 
-    t.amount, 
-    ROUND(t.amount/7, 2) average_amount
-FROM (
-    SELECT 
-        DISTINCT visited_on, 
-        SUM(amount) OVER(ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) amount, 
-        MIN(visited_on) OVER() AS FIRST_DATE
-    FROM Customer
-) t
-WHERE t.visited_on >= DATE_ADD(FIRST_DATE, INTERVAL 6 DAY) ;
+SELECT
+    visited_on,
+    (
+        SELECT SUM(amount)
+        FROM customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ) AS amount,
+    ROUND(
+        (
+            SELECT SUM(amount) / 7
+            FROM customer
+            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+        ),2 ) AS average_amount
+FROM customer c
+WHERE visited_on >= (
+        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+        FROM customer
+    )
+GROUP BY visited_on;
