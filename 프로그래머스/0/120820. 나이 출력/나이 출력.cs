5be63fb023fd93b1c3426c4d99c2556191933eab
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2025-01-15 14:04:58 +0900

    [level 0] Title: 나이 출력, Time: 0.14 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.cs
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int age) {
-        int answer = 2022 - age+1;
+        int answer = 2022 - age + 1;
         return answer;
     }
 }
