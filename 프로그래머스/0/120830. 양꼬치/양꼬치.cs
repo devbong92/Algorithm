54ff9d2e70720813f7043610c3b96b3133b3e992
Jeon BongJun <95843762+devbong92@users.noreply.github.com>
2025-07-18 10:51:05 +0900

    [level 0] Title: 양꼬치, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int LAMB_SKEWER = 12000;
+        int DRINK = 2000;
+        int answer = (LAMB_SKEWER * n) + (DRINK * (k - (n / 10)) );
+        return answer;
+    }
+}
